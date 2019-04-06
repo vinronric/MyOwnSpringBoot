d2c0fabad90486ea0187a3cd42e7d7491c44339a
User <User@10.0.0.135>
2019-04-06 12:56:00 -0400

    moved 1st set of files

---
+++ demospringboot/src/main/java/com/example/demospringboot/DemospringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringbootApplication.class, args);
+		System.out.println("Welcome Spring Boot");
+	}
+
+}
---
+++ demospringboot/src/main/java/com/example/demospringboot/Test.java
@@ -0,0 +1,10 @@
+package com.example.demospringboot;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hi Vinoth");
+	}
+
+}
